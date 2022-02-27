2a48616905d5091c5346d6f3ac9ce71e56677755
Bbanez <banezprod@gmail.com>
2022-02-27 12:33:22 +0100

    Export all

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 export * from './camera';
+export * from './math';
 export * from './util';
 
 export * from './consts';
+export * from './entity';
 export * from './keyboard';
 export * from './loader';
 export * from './logger';
 export * from './main';
 export * from './mouse';
 export * from './renderer';
+export * from './scene';
 export * from './storage';
 export * from './ticker';
